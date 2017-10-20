a74e665c8e87a2fb7b0194a2a04fdd7ac2a77611
vincecampanale <vcampanale0294@gmail.com>
2017-10-19 21:43:40 -0400

    Login/Register reactive form components [6:34]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
+import { AuthModule } from '../auth/auth.module';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +32,14 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+var config = {
+  apiKey: "AIzaSyA2bpwMze0L--e61iM6zMtnBVmzmwnFb70",
+  authDomain: "fitness-app-221c2.firebaseapp.com",
+  databaseURL: "https://fitness-app-221c2.firebaseio.com",
+  projectId: "fitness-app-221c2",
+  storageBucket: "fitness-app-221c2.appspot.com",
+  messagingSenderId: "678725160327"
+};
+*/
---
+++ src/auth/shared/components/auth-form/auth-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-form',
+  styleUrls: ['auth-form.component.scss'],
+  template: `
+    <div>
+      Auth Form! 
+    </div>
+  `
+})
+export class AuthFormComponent {
+  constructor() {}
+}
