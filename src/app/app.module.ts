8493d5d86e6e1a1548783e2355995955b0a4c95c
vincecampanale <vcampanale0294@gmail.com>
2017-10-23 13:39:14 -0400

    wo[

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { Store } from 'store';
 
 // feature modules
 import { AuthModule } from '../auth/auth.module';
+import { HealthModule } from '../health/health.module';
+
 
 // containers
 import { AppComponent } from './containers/app/app.component';
 
 // components
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppNavComponent } from './components/app-nav/app-nav.component';
 
 // routes
 export const ROUTES: Routes = [];
@@ -19,10 +23,13 @@ export const ROUTES: Routes = [];
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES),
-    AuthModule
+    AuthModule,
+    HealthModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppNavComponent
   ],
   providers: [
     Store
@@ -34,12 +41,4 @@ export const ROUTES: Routes = [];
 export class AppModule {}
 
 /*
-var config = {
-  apiKey: "AIzaSyA2bpwMze0L--e61iM6zMtnBVmzmwnFb70",
-  authDomain: "fitness-app-221c2.firebaseapp.com",
-  databaseURL: "https://fitness-app-221c2.firebaseio.com",
-  projectId: "fitness-app-221c2",
-  storageBucket: "fitness-app-221c2.appspot.com",
-  messagingSenderId: "678725160327"
-};
 */
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
 export const ROUTES: Routes = [
   {
     path: 'auth',
@@ -13,10 +21,23 @@ export const ROUTES: Routes = [
   }
 ];
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2bpwMze0L--e61iM6zMtnBVmzmwnFb70",
+  authDomain: "fitness-app-221c2.firebaseapp.com",
+  databaseURL: "https://fitness-app-221c2.firebaseio.com",
+  projectId: "fitness-app-221c2",
+  storageBucket: "fitness-app-221c2.appspot.com",
+  messagingSenderId: "678725160327"
+};
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    SharedModule.forRoot()
   ]
 })
 export class AuthModule {}
