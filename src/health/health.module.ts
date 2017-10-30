514d6e70c8dac699278584b4e699be98cc252875
Vince Campanale <vcampanale0294@gmail.com>
2017-10-29 21:22:18 -0400

    HealthModule setup and lazy loading 9:39

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
-  { path: 'meals', loadChildren: './meals/meals.module#MealsModule' }
+  { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
+  { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' },
+  { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' }
 ]
 
 @NgModule({
