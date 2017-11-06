746aac4dae9479fd05434de075988b25998eb33c
vincecampanale <vcampanale0294@gmail.com>
2017-11-05 20:07:15 -0500

    Data layer, initiate Observable streams 4:42

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// shared
+import { SharedModule } from './shared/shared.module';
+
+// guards
+import { AuthGuard } from './../auth/shared/guards/auth.guard';
+
 export const ROUTES: Routes = [
-  { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
-  { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' },
-  { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' }
+  {
+    path: 'meals',
+    canActivate: [AuthGuard],
+    loadChildren: './meals/meals.module#MealsModule'
+  },
+  {
+    path: 'workouts',
+    canActivate: [AuthGuard],
+    loadChildren: './workouts/workouts.module#WorkoutsModule'
+  },
+  {
+    path: 'schedule',
+    canActivate: [AuthGuard],
+    loadChildren: './schedule/schedule.module#ScheduleModule'
+  }
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    SharedModule.forRoot()
   ]
 })
-export class HealthModule {}
+export class HealthModule { }
